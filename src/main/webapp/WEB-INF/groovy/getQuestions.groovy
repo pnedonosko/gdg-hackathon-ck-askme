6e0871e7568d80352903ff998f63154de86ee61f
Peter Nedonosko <pnedonosko@gmail.com>
2013-02-23 20:06:35 +0200

    questions service (in progress)

---
+++ src/main/webapp/WEB-INF/groovy/getQuestions.groovy
@@ -0,0 +1,26 @@
+import ua.org.gdg.cherkasy.hackathon.askme.*
+
+import ua.org.gdg.cherkasy.hackathon.askme.translation.GoogleTranslator
+import ua.org.gdg.cherkasy.hackathon.askme.translation.Languages
+import ua.org.gdg.cherkasy.hackathon.askme.translation.TextEntry
+
+def translator = new GoogleTranslator()
+//def period = params.p
+//def json
+//def where
+//if (period) {
+	//where = "created > $period"
+//}
+//def result = translator.translate(Languages.UK, new TextEntry(Languages.EN, params.text))
+
+def targetLang = params.lang
+def questions = []
+for (q in Question.findAll()) {
+	log.info "Question ${q.text}"
+	q.text = translator.translate(targetLang, new TextEntry(q.lang, params.text))
+	questions << q
+} 
+
+//json = jsonLibBuilderFactory.jsonLibBuilder.json { id = user.deviceId }
+response.renderJson questions
+
